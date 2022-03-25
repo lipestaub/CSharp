07a4837679d56fb637b3306e25b32b0b6a3d7b2c
lipestaub <86986800+lipestaub@users.noreply.github.com>
2022-03-25 11:10:02 -0300

    Add files via upload

---
+++ ProjetosC#/ProjetoComSelect/ConexaoBD.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace ProjetoComSelect
+{
+    public class ConexaoBD
+    {
+        // privado somente leitura  tipo  nome      recebe nova instancia
+        private readonly static ConexaoBD conexaoBD = new ConexaoBD();
+
+        // obtem uma instancia da conexao.
+        public static ConexaoBD GetInstancia()
+        {
+            return conexaoBD;
+        }
+
+        // retorna a conexão do MySql
+        public MySqlConnection GetConexao()
+        {
+            // acessa o arquivo de configuracao chamada App.config
+            // e lê a string de conexao
+            string con = ConfigurationManager
+                .ConnectionStrings["MySQLConnectionString"].ToString();
+            // criar uma conexão com o banco de dados
+            MySqlConnection conexao = new MySqlConnection(con);
+
+            return conexao;
+        } 
+    }
+}
